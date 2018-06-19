817070aab830a5f16d9b9fc0fddd519aa3d4f5f1
lganz <lganz@seibert-media.net>
2018-06-19 14:06:02 +0200

    plus / minus function

--- app.js
+++ app.js
@@ -1,55 +1,55 @@
-$(document).ready(function() {
-  //inputs stores inputs to calc later
-  var inputs = [];
-  //output stores values (outputs), shown on display
-  var output = [];
+$(document).ready(function () {
+	//inputs stores inputs to calc later
+	var inputs = [];
+	//output stores values (outputs), shown on display
+	var output = [];
 
-  //totalString string stores current input string
-  var totalString;
-  //operators array with all operators and modifiers
-  var operators = ["+", "-", "÷", "/", "%", "*", "x", "."];
-  //digits array with digits
-  var digits = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
-  //symbols stores operators as well as digits
-  var symbols = digits.concat(operators);
+	//totalString string stores current input string
+	var totalString;
+	//operators array with all operators and modifiers
+	var operators = ["+", "-", "÷", "/", "%", "*", "x", "."];
+	//digits array with digits
+	var digits = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+	//symbols stores operators as well as digits
+	var symbols = digits.concat(operators);
 
 //getValue checks whether input value is insecure, if so return
 //pushes new value in inputs-array
 //joins elements of array into string and stores it in totalString
-  function getValue(input) {
-    if ( ! isSecure(input) ) {
-      return;
-    }
-    inputs.push(input);
-    totalString = inputs.join("");
-    console.log(totalString);
-  }
+	function getValue (input) {
+		if (!isSecure(input)) {
+			return;
+		}
+		inputs.push(input);
+		totalString = inputs.join("");
+		console.log(totalString);
+	}
 
 //displayValues checks whether currentValue value is insecure, is so return
 //else currentValue gets pushed into output array
 //output.join(""): joins the elements of output in a String (outputString)
 //calls showDigit with outputString as parameters
 
-  function displayValues( currentValue ) {
-    if ( ! isSecure(currentValue) ) {
-      return;
-    }
-    output.push(currentValue);
-    outputString = output.join("");
-    showDigit(outputString);
-    console.log(outputString); //debugging
-  }
+	function displayValues (currentValue) {
+		if (!isSecure(currentValue)) {
+			return;
+		}
+		output.push(currentValue);
+		outputString = output.join("");
+		showDigit(outputString);
+		console.log(outputString); //debugging
+	}
 
 //isSecure parameter value is converted to String
 //returns value if it's included in symbols
-  function isSecure( value ) {
-    return symbols.includes( value.toString() );
-  }
+	function isSecure (value) {
+		return symbols.includes(value.toString());
+	}
 
 //showDigit displays committed parameters
-  function showDigit( digit ) {
-    $(".item-header").html(digit);
-  }
+	function showDigit (digit) {
+		$(".item-header").html(digit);
+	}
 
 //getTotal checks if totalString is empty (no button has been clicked before)
 //if so return
@@ -57,33 +57,37 @@ $(document).ready(function() {
 //displays result with two decimal places
 //stores result in inputs array
 //stores result in output array
-  function getTotal() {
-    if ( ! totalString) {
-      return;
-    }
-    var result = eval(totalString);
-    result = result.toFixed(2);
-    showDigit( result );
-    console.log('result', result); //debugging
-    inputs = [result];
-    output = [result];
-  }
+	function getTotal () {
+		if (!totalString) {
+			return;
+		}
+		var result = eval(totalString);
+		result = result.toFixed(2);
+		showDigit(result);
+		console.log('result', result); //debugging
+		inputs = [result];
+		output = [result];
+	}
 
 
-  function percentage( ) {
-    if (! isSecure( )) {
-      return;
-    }
-    totalString = "(" + eval(totalString) + ")/100";
-    getTotal();
-  }
+	function percentage () {
+		if (!isSecure()) {
+			return;
+		}
+		totalString = "(" + eval(totalString) + ")/100";
+		getTotal();
+	}
 
+	function leadingsign () {
+		inputs[inputs.length - 1] = inputs.length !== 0 ? inputs[inputs.length - 1] * (-1) : 0;
+		showDigit(inputs[inputs.length - 1]);
+	}
 
 //resetValues empties inputs string & output string
-  function resetValues() {
-    inputs = [""];
-    output = [""];
-  }
+	function resetValues () {
+		inputs = [""];
+		output = [""];
+	}
 
 //click-function: is() determines, if the two values (from $this and data-attribute number) are the same value
 //if true and totalString is empty, return
@@ -93,39 +97,40 @@ $(document).ready(function() {
 //default : displayValues 'this'
 //          getValue gets data-attributes 'data'
 
-  $("[data-button]").click(function() {
-    if ( ! $(this).is('[data-button-number]') && ! totalString ) {
-      return;
-    }
+	$("[data-button]").click(function () {
+		if (!$(this).is('[data-button-number]') && !totalString) {
+			return;
+		}
 
-    switch (this.id) {
-      case 'clearall':
-        resetValues();
-        showDigit(0);
-        break;
+		switch (this.id) {
+			case 'clearall':
+				resetValues();
+				showDigit(0);
+				break;
 
-      case 'equals':
-        getTotal();
-        break;
+			case 'equals':
+				getTotal();
+				break;
 
-      case 'percentage':
-         console.log("percentage event clicked");
-		  percentage();
-         break;
+			case 'leadingsign':
+				leadingsign();
+				break;
 
-      default:
-        displayValues( $(this).html() );
-        getValue( $(this).data('value') );
-    }
-  });
+			case 'percentage':
+				console.log("percentage event clicked");
+				percentage();
+				break;
+
+			default:
+				displayValues($(this).html());
+				getValue($(this).data('value'));
+		}
+	});
 });
 
 
-
-
-
 /***fiddle
-if ($(this).is("#percentage")) {
+ if ($(this).is("#percentage")) {
   console.log("percentage clicked");
   result = result / 100;
 }
@@ -135,8 +140,8 @@ if ($(this).is("#percentage")) {
 
 
 
-//join() joins the elements of an array into a string
-  // function update() {
+ //join() joins the elements of an array into a string
+ // function update() {
   //   totalString = output.join("");
   //   $(".item-header").html(totalString);
   //
@@ -144,8 +149,8 @@ if ($(this).is("#percentage")) {
 
 
 
-  // //*leadingsign function*: ---
-  //   $("#leadingsign").click(function(){
+ // //*leadingsign function*: ---
+ //   $("#leadingsign").click(function(){
   //     if (this.id === "leadingsign"){
   //       console.log("vorzeichen");
   //
@@ -153,6 +158,6 @@ if ($(this).is("#percentage")) {
   //       //}
   //
   //     }
-  //   });
+ //   });
 
-*/
+ */
