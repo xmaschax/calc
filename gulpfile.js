628023eaa1070d516ba30add24020907d047bb55
lganz <lganz@seibert-media.net>
2018-06-21 11:26:31 +0200

    set correct relative path

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,7 @@ var gulp = require('gulp');
 var sass = require('gulp-sass');
 
 var sassFiles = './*.scss',
-	cssDest = './css/';
+	cssDest = './';
 
 gulp.task('sass', function () {
 	return gulp.src(sassFiles)
